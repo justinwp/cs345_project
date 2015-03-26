6083b8a07d8773818f5ac1e8f93a961c6d962a56
Justin Poehnelt <justin.poehnelt@gmail.com>
2015-03-26 12:50:15 -0700

    initial commit

---
+++ init.sql
@@ -0,0 +1,57 @@
+/*
+
+Group Project IV Initialization Script
+
+>> set echo on
+>> @cs345-pr-init.sql
+
+ */
+
+
+
+/*
+*************
+Drop Tables
+*************
+
+Start from a clean database. See below for prefixing so that homework tables are not dropped and order of drop
+*/
+
+-- DELETE FROM animal;
+-- DROP TABLE animal;
+
+
+
+/*
+*************
+Create Tables
+*************
+
+TODO should we prefix our tables so that they do not collide with homework tables? for example shelter_employees
+TODO naming pattern for constraint
+
+primary key:          tablename_columnname_pk
+foreign key:          tablename_columnname_pk
+unique constraint:    tablename_columnname_uq
+not null constraint:  tablename_columnname_nn
+value constraint:     tablename_columnname_val_description
+
+Order of Creation Based on References:
+  1.
+  2.
+  3.
+  ...
+
+ */
+
+
+
+/*
+*****************
+Add Data to Table
+*****************
+ */
+
+
+
+--- Add data
