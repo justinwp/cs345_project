8a129cacb4a40e239eb5e7a0d8ed8d2e1b218e6f
Justin Poehnelt <justin.poehnelt@gmail.com>
2015-04-21 12:43:25 -0700

    added a few tables

--- init.sql
+++ init.sql
@@ -3,7 +3,7 @@
 Group Project IV Initialization Script
 
 >> set echo on
->> @cs345-pr-init.sql
+>> @init.sql
 
  */
 
@@ -17,8 +17,10 @@ Drop Tables
 Start from a clean database. See below for prefixing so that homework tables are not dropped and order of drop
 */
 
--- DELETE FROM animal;
 -- DROP TABLE animal;
+-- DROP TABLE kennel;
+-- DROP TABLE animal_subtype;
+-- DROP TABLE animal_type;
 
 
 
@@ -26,24 +28,43 @@ Start from a clean database. See below for prefixing so that homework tables are
 *************
 Create Tables
 *************
-
-TODO should we prefix our tables so that they do not collide with homework tables? for example shelter_employees
-TODO naming pattern for constraint
-
-primary key:          tablename_columnname_pk
-foreign key:          tablename_columnname_pk
-unique constraint:    tablename_columnname_uq
-not null constraint:  tablename_columnname_nn
-value constraint:     tablename_columnname_val_description
-
-Order of Creation Based on References:
-  1.
-  2.
-  3.
-  ...
-
  */
 
+CREATE TABLE animal_type (
+  animal_type_id          NUMBER(10)   CONSTRAINT animal_type_id_nn NOT NULL DEFERRABLE INITIALLY IMMEDIATE,
+  animal_type_name        VARCHAR2(50) CONSTRAINT animal_type_name_nn NOT NULL DEFERRABLE INITIALLY IMMEDIATE,
+  animal_type_description VARCHAR2(255),
+  CONSTRAINT animal_type_id_pk PRIMARY KEY (animal_type_id) DEFERRABLE INITIALLY IMMEDIATE,
+  CONSTRAINT animal_subtype_id_fk FOREIGN KEY (animal_type_id) REFERENCES animal_type (animal_type_id) DEFERRABLE INITIALLY IMMEDIATE
+);
+
+CREATE TABLE animal_subtype (
+  animal_subtype_id          NUMBER(10)   CONSTRAINT animal_subtype_id_nn NOT NULL DEFERRABLE INITIALLY IMMEDIATE,
+  animal_type_id             NUMBER(10)   CONSTRAINT animal_subtype_type_id_nn NOT NULL DEFERRABLE INITIALLY IMMEDIATE,
+  animal_subtype_name        VARCHAR2(50) CONSTRAINT animal_subtype_name_nn NOT NULL DEFERRABLE INITIALLY IMMEDIATE,
+  animal_subtype_description VARCHAR2(255),
+  CONSTRAINT animal_subtype_id_pk PRIMARY KEY (animal_subtype_id) DEFERRABLE INITIALLY IMMEDIATE,
+  CONSTRAINT animal_subtype_type_id_fk FOREIGN KEY (animal_type_id) REFERENCES animal_type (animal_type_id) DEFERRABLE INITIALLY IMMEDIATE
+);
+
+CREATE TABLE kennel (
+  kennel_id      NUMBER(10) CONSTRAINT animal_type_animal_type_id_nn NOT NULL DEFERRABLE INITIALLY IMMEDIATE,
+  kennel_space   NUMBER(4),
+  animal_type_id NUMBER(10),
+  CONSTRAINT kennel_id_pk PRIMARY KEY (kennel_id) DEFERRABLE INITIALLY IMMEDIATE,
+  CONSTRAINT kennel_animal_type_id_fk FOREIGN KEY (animal_type_id) REFERENCES animal_type (animal_type_id) DEFERRABLE INITIALLY IMMEDIATE
+);
+
+CREATE TABLE animal (
+  animal_id           NUMBER(10) CONSTRAINT animal_animal_id_nn NOT NULL DEFERRABLE INITIALLY IMMEDIATE,
+  animal_subtype_id   NUMBER(10) CONSTRAINT animal_animal_subtype_id_nn NOT NULL DEFERRABLE INITIALLY IMMEDIATE,
+  kennel_id           NUMBER(10),
+  animal_name         VARCHAR2(50),
+  animal_arrival_date DATE DEFAULT CURRENT_DATE,
+  CONSTRAINT animal_animal_id_pk PRIMARY KEY (animal_id) DEFERRABLE INITIALLY IMMEDIATE,
+  CONSTRAINT animal_animal_subtype_id_fk FOREIGN KEY (animal_subtype_id) REFERENCES animal_subtype (animal_subtype_id) DEFERRABLE INITIALLY IMMEDIATE,
+  CONSTRAINT animal_kennel_id_fk FOREIGN KEY (kennel_id) REFERENCES kennel (kennel_id) DEFERRABLE INITIALLY IMMEDIATE
+);
 
 
 /*
@@ -52,6 +73,3 @@ Add Data to Table
 *****************
  */
 
-
-
---- Add data
